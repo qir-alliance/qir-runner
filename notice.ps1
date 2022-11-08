6a4f3f6756d98911a227eb9470abd61044535e3f
Ian Davis <ian.f.davis@gmail.com>
2022-11-08 16:23:26 +0000

    Adding notices and artifact saving

---
+++ notice.ps1
@@ -0,0 +1,32 @@
+#!/usr/bin/env pwsh
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+#Requires -PSEdition Core
+
+function update-noticefiles {
+    # use cargo-about to generate a notice files
+    # notice files are only for wheel distributions
+    # as no bundled sources are in the sdist.
+
+    # llvm special license is already in the template
+    # as it is a hidden transitive dependency.
+    # https://github.com/EmbarkStudios/cargo-about
+    $config = Join-Path $PSScriptRoot notice.toml
+    $template = Join-Path $PSScriptRoot notice.hbs
+    foreach ($project in @("runner")) {
+        Push-Location $project
+        try {
+            $notice = Join-Path $PSScriptRoot $project NOTICE.txt
+            cargo about generate --config $config --all-features --output-file $notice $template
+            $contents = Get-Content -Raw $notice
+            [System.Web.HttpUtility]::HtmlDecode($contents) | Out-File $notice
+        }
+        finally {
+            Pop-Location
+        }
+    }
+}
+
+update-noticefiles
