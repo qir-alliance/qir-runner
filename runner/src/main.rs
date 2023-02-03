13d9e389e06ad88b6a7edf71e06edc400d119c21
Stefan J. Wernli <swernli@microsoft.com>
2023-02-03 08:33:44 -0800

    Add rngseed cmdline parameter (#32)
    * Add rngseed cmdline parameter
    
    This change adds the `--rngseed` or `-r` cmdline parameter to the runner that can allow callers to seed the thread-local pseudo-random number generator used during quantum simulation. This can be handy for testing purposes by increasing predictability. Note that the seed is set outside of the shot loop, so that multiple shots should still be distinct but the sequence of results for the shots should always be the same for the same seed.
    
    Also note that this only sets the seed used for quantum randomness. The classical randomness provided by `__quantum__qis__drawrandomint__body` and `__quantum__qis__drawrandomdouble__body` in the standard library do not use the seeded rng and remain non-deterministically random.
    
    * Adding runner command line usage to readme
    
    ---------
    
    Co-authored-by: Ian Davis <Ian.F.Davis@microsoft.com>

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -16,7 +16,9 @@ fn main() -> Result<(), String> {
         arg!(-e --entrypoint <NAME> "Name of the entry point function to execute"),
         arg!(-s --shots <NUM> "The number of times to repeat the execution of the chosen entry point in the program")
             .value_parser(value_parser!(u32))
-            .default_value("1")]);
+            .default_value("1"),
+        arg!(-r --rngseed <NUM> "The value to use when seeding the random number generator used for quantum simulation")
+            .value_parser(value_parser!(u64))]);
 
     let matches = cmd.try_get_matches().map_err(|e| e.to_string());
     match matches {
@@ -24,12 +26,18 @@ fn main() -> Result<(), String> {
             eprint!("{}", e);
             Ok(())
         }
-        Ok(matches) => qir_runner::run_file(
-            matches.get_one::<PathBuf>("file").unwrap(),
-            matches
-                .get_one::<String>("entrypoint")
-                .map(std::string::String::as_str),
-            *matches.get_one::<u32>("shots").unwrap(),
-        ),
+        Ok(matches) => {
+            if let Ok(Some(seed)) = matches.try_get_one::<u64>("rngseed") {
+                qir_backend::set_rng_seed(*seed);
+            }
+
+            qir_runner::run_file(
+                matches.get_one::<PathBuf>("file").unwrap(),
+                matches
+                    .get_one::<String>("entrypoint")
+                    .map(std::string::String::as_str),
+                *matches.get_one::<u32>("shots").unwrap(),
+            )
+        }
     }
 }
