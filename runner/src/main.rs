af637199e6d5295bbcdd122371cc506b99867905
11fwojcieszy <filip.wojcieszyn@sonova.com>
2023-06-27 15:09:24 +0200

    added version command

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -5,8 +5,7 @@
 
 use std::path::PathBuf;
 
-use clap::{arg, value_parser, Command};
-// use std::env;
+use clap::{arg, crate_version, value_parser, Command};
 
 fn main() -> Result<(), String> {
     let cmd = Command::new("qir-runner").args(&[
@@ -18,7 +17,8 @@ fn main() -> Result<(), String> {
             .value_parser(value_parser!(u32))
             .default_value("1"),
         arg!(-r --rngseed <NUM> "The value to use when seeding the random number generator used for quantum simulation")
-            .value_parser(value_parser!(u64))]);
+            .value_parser(value_parser!(u64))
+        ]).version(crate_version!());
 
     let matches = cmd.try_get_matches().map_err(|e| e.to_string());
     match matches {
