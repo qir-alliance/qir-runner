c39a08a8be89f07cb86f5c74a124931db2f27c00
Ian Davis <ian.f.davis@microsoft.com>
2024-01-29 09:13:14 -0800

    Adding Python wheel command line support

---
+++ pip/qir_runner/_native.pyi
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from typing import List, Optional
+
+def run_file(path: str, entry_point: Optional[str], shots: Optional[int]): ...
+def main(args: Optional[List[str]]): ...
---
+++ pip/src/python.rs
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+use std::ffi::OsString;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+#[pyo3(text_signature = "(path, entry_point, shots)")]
+#[allow(clippy::needless_pass_by_value)]
+pub(crate) fn run_file(
+    _py: Python,
+    path: String,
+    entry_point: Option<String>,
+    shots: Option<u32>,
+) -> PyResult<()> {
+    runner::run_file(path, entry_point.as_deref(), shots.unwrap_or(1))
+        .map_err(|msg| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(msg))?;
+    Ok(())
+}
+
+#[pyfunction]
+#[pyo3(text_signature = "(args)")]
+pub(crate) fn main(args: Option<Vec<String>>) -> PyResult<()> {
+    match args {
+        Some(args) => {
+            let args: Vec<OsString> = args.into_iter().map(|s| s.into()).collect::<Vec<_>>();
+            runner::main(Some(args))
+                .map_err(|msg| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(msg))?;
+        }
+        None => {
+            // skip the first arg, which is the name of the python executable
+            let args = std::env::args_os().skip(1);
+            runner::main(Some(args))
+                .map_err(|msg| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(msg))?;
+        }
+    }
+
+    Ok(())
+}
+
+#[pymodule]
+fn _native(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(run_file, m)?)?;
+    m.add_function(wrap_pyfunction!(main, m)?)?;
+
+    Ok(())
+}
