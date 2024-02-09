24356bd70f6095dd39bd1508c6a3f70433c8df62
swernli <swernli@microsoft.com>
2024-02-09 19:28:41 +0000

    Deploying to gh-pages from @ qir-alliance/qir-runner@6b5ccb346239082a68c97e0198ea36d7407116f5 🚀

--- src-files.js
+++ src-files.js
@@ -1,7 +1,7 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "qir_backend":["",[],["exp.rs","lib.rs","result_bool.rs"]],\
 "qir_runner":["",[],["lib.rs"]],\
 "qir_stdlib":["",[],["arrays.rs","bigints.rs","callables.rs","lib.rs","math.rs","output_recording.rs","range_support.rs","strings.rs","tuples.rs"]],\
 "quantum_sparse_sim":["",[],["exp.rs","lib.rs","nearly_zero.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
