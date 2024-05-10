1db093761ef118b9d3dfc73324640f7c9e5b5c58
Ian Davis <ian.f.davis@microsoft.com>
2024-05-10 14:16:14 -0700

    Add use statement instead of full qualification

--- stdlib/src/lib.rs
+++ stdlib/src/lib.rs
@@ -25,6 +25,8 @@ use std::{
     rc::{Rc, Weak},
 };
 
+use output_recording::record_output_str;
+
 /// Utility used for managing refcounted items.
 unsafe fn update_counts<T>(raw_rc: *const T, update: i32, is_alias: bool) {
     let mut remaining = update;
@@ -86,7 +88,7 @@ pub unsafe extern "C" fn __quantum__rt__fail(str: *const CString) {
 #[cfg(feature = "message-support")]
 #[no_mangle]
 pub unsafe extern "C" fn __quantum__rt__message(str: *const CString) {
-    crate::output_recording::record_output_str(&format!(
+    record_output_str(&format!(
         "INFO\t{}",
         (*str)
             .to_str()
