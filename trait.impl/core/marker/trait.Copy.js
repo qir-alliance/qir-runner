4afd155af2ba41a93de6b051877a5c41ffd27101
swernli <swernli@microsoft.com>
2025-04-07 21:04:00 +0000

    Deploying to gh-pages from @ qir-alliance/qir-runner@999d56bb1e65c6443b611dc14271692fdc228cb4 🚀

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["qir_stdlib",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"qir_stdlib/enum.Pauli.html\" title=\"enum qir_stdlib::Pauli\">Pauli</a>"]]]]);
+    var implementors = Object.fromEntries([["qir_stdlib",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"qir_stdlib/enum.Pauli.html\" title=\"enum qir_stdlib::Pauli\">Pauli</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
